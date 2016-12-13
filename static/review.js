32945a40939bedbe36957284599e0de8b0bbdf79
leeb2018 <lukelee92@gmail.com>
2016-12-12 19:58:38 -0500

    semi colons added to review.js

--- static/review.js
+++ static/review.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
 	$('.restaurant_review_container').click(function() {
-  		var newReview = "No review available. :("
+  		var newReview = "No review available. :(";
         $.ajax({
             url: '/getReview',
             type: 'POST',
             success: function(response) {
-            	var kvPair = response["review"]
-            	newReview = kvPair[1]
-                $('.restaurant_review_container').html(newReview)
+            	var kvPair = response["review"];
+            	newReview = kvPair[1];
+            	console.log(newReview);
+                $('.restaurant_review_container').html(newReview);
             },
             error: function(error) {
                 console.log(error);
